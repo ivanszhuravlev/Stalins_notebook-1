3d1a32ad4519b2cab5537ae789530e59a7590085
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2016-05-14 15:17:12 +0300

    Added regexp for Telephone1

--- Stalins_notebook/scripts/controllers/contactController.js
+++ Stalins_notebook/scripts/controllers/contactController.js
@@ -9,11 +9,20 @@ mainApp.controller("contactController", function ($scope, $http) {
     }
 
     $scope.addContact = function (contact, addContactForm) {
+        var phone_format = /\+7\d{3}-\d{3}-\d{2}-\d{2}/;
 
-        $http.post("/api/Contacts",contact).success(function (data) {
-            console.log(data);
-            $scope.model.contacts.unshift(data);
-        });
+        if (phone_format.exec(contact.Telephone1)) {
+            contact.Telephone1 = contact.Telephone1.replace(/-/g, "");
+
+            alert(contact.Telephone1);
+
+            $http.post("/api/Contacts", contact).success(function (data) {
+                console.log(data);
+                $scope.model.contacts.unshift(data);
+            });
+        } else {
+            alert("fail");
+        }
 
         $scope.path = '';
     }
--- Stalins_notebook/scripts/controllers/listController.js
+++ Stalins_notebook/scripts/controllers/listController.js
@@ -176,16 +176,4 @@ mainApp.controller("listController", function ($scope, $http) {
             action_bar.classList.remove("shown");
         }
     }
-
-    /*-------------------------------------------------------*/
-
-    $scope.call = function () {
-        alert("hi");
-        Skype.ui({
-            name: "call",
-            element: "SkypeButton_Call_#selisej198_1",
-            participants: ["selisej198"],
-            video: false
-        });
-    }
 });
